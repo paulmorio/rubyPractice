f61b5b773ed15816d5a4e7618679e76ecf32a2ea
paulmorio <paulmorio@github.com>
2014-07-11 18:35:02 +0200

    ruby practice day 2

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you in centimetres? "
+height = gets.chomp()
+print "How much do you weight in kilograms? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} centimeters tall and #{weight} kilograms heavy."
